49cd0af07261b0e99e5bbb331249a237df954ed5
dfgs <dfgs83@gmail.com>
2025-07-21 11:34:10 +0200

    fixed unit tests for tables without PK

---
+++ LibraryExample.UnitTests/TableWithoutPKUnitTest.cs
@@ -0,0 +1,16 @@
+using LibraryExample.Models;
+using DataModelLib;
+using System.Reflection;
+using System.Diagnostics.Tracing;
+using BlueprintLib.Attributes;
+
+namespace LibraryExample.UnitTests
+{
+	[DTO("TableWithoutPK"), MockCount(6), TestClass]
+	public class TableWithoutPKUnitTest
+	{
+	
+
+
+	}
+}
---
+++ LibraryExample/TableWithoutPK.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DataModelLib;
+using System.Diagnostics.CodeAnalysis;
+using BlueprintLib.Attributes;
+
+namespace LibraryExample
+{
+	[DTO("TableWithoutPK"),Blueprint("DTO"), Blueprint("TableModel")]
+	public partial class TableWithoutPK
+	{
+		
+		
+
+
+	}
+}
